ee05df07592d8962368191aa6c9602b232d47166
Pato-Bett <patobett588@gmail.com>
2023-11-06 04:16:28 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form')
+  const tasksList = document.querySelector('#tasks')
+
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const taskInput = document.getElementById('new-task-description').value
+    console.log(taskInput)
+    const task = document.createElement('li')
+    task.textContent = taskInput
+    // tasksList.appendChild(task)
+    tasksList.innerHTML += `<li>${taskInput}</li>`
+    document.querySelector('#new-task-description').value = ""
+  })
 });
